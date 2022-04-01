6e1a899bc5b0347124287ae2add080cd97f26a4c
Daniel Chavez <programasfulltech@gmail.com>
2022-04-01 10:22:34 +0200

    ft_strjoin.c with include && start of ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+char *ft_strtrim(char const *s1, char const *set)
+{
+    char* s3;
+    char* s2;
+    int s1len;
+    int setlen;
+    setlen = ft_strlen(set);
+    s1len = ft_strlen(s1);
+    int i;
+    while (
+        s1[i] != set[i]
+    )
+    {
+        
+    }
+    
+}
