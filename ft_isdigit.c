13f679c8f7866d3c38a4b3a6424f6cb9ece4fe82
Daniel Chavez <programasfulltech@gmail.com>
2022-04-01 09:43:19 +0200

    ft_substr.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -12,12 +12,12 @@
 #include "libft.h"
 int ft_isdigit(int c)
 {
-	if(c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
