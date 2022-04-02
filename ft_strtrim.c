c4c7b57687f425a0b569b5f23e9f9162b988e807
Daniel oreste Chavez orribo <dchavez-@car4s3.42barcelona.com>
2022-04-02 22:07:44 +0200

    copiedfromlocal42

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,8 @@
 /*   By: dchavez- <dchavez-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 21:46:07 by dchavez-          #+#    #+#             */
-/*   Updated: 2022/04/02 21:52:18 by dchavez-         ###   ########.fr       */
+/*   Updated: 2022/04/02 21:57:13 by dchavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-char *ft_strtrim(char const *s1, char const *set)
-{
-    char* s3;
-    char* s2;
-    int s1len;
-    int setlen;
-    setlen = ft_strlen(set);
-    s1len = ft_strlen(s1);
-    int i;
-    while (
-        s1[i] != set[i]
-    )
-    {
-        
-    }
-    
-}
+
