fab775a76bbd49c52d2ac6ae61edf92c305394b1
Daniel oreste Chavez orribo <dchavez-@car2s4.42barcelona.com>
2022-04-03 22:43:33 +0200

    sun3/4/2022

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,11 @@
 /*   By: dchavez- <dchavez-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 21:46:07 by dchavez-          #+#    #+#             */
-/*   Updated: 2022/04/03 21:04:53 by dchavez-         ###   ########.fr       */
+/*   Updated: 2022/04/03 22:42:53 by dchavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-char *ft_strtrim(char const *s1, char const *set)
+/*char *ft_strtrim(char const *s1, char const *set)
 {
     char            *res;
     unsigned long   setlen;
@@ -22,4 +22,45 @@ char *ft_strtrim(char const *s1, char const *set)
         return(NULL);
     ft_strlcpy(res,s1+setlen,ft_strlen(s1)-2*setlen+1);
     return(res);
+}
+*/
+char *ft_moveleft(char	*s,int index)
+{
+				int z;
+				z=index;
+				while(s[z])
+				{
+				s[z] = s[z+1];
+				z++;
+				}
+				return(s);
+}
+char *ft_strtrim(char const *s1, char const *set)
+{
+	char	*res;
+	int i;
+	int u;
+	int len;
+	i=0;
+	len= ft_strlen(s1)-1;
+	while(s1[i])
+	{
+		u=0;
+		if(s1[i]==s1[i+1] || (s1[i] ==set[u] && s1[i+1]==set[u+1])
+				|| (s1[i] ==set[u+1] && s1[i+1]==set[u]))
+		{
+			while(s1[i]==set[u] && set[u] != '\0')
+			{
+				ft_moveleft((char	*)s1,i);
+				i=0;
+				u++;
+			}
+		}
+	i++;
+	}
+	res=(char *)calloc(ft_strlen(s1),ft_strlen(s1)+1);
+    if (res==NULL)
+        return(NULL);
+	ft_strlcpy(res,s1,len);
+	return(res);
 }
